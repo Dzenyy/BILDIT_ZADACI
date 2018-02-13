999b5972df1a00bef803be24c15188cdaded7b4b
Dzenita Dulovic <djulovic.dzenita1999@gmail.com>
2018-02-13 09:08:32 +0100

    done

--- src/zadaci_12_02_2018/NajvecaVrijednostArrayList.java
+++ src/zadaci_12_02_2018/NajvecaVrijednostArrayList.java
@@ -1,8 +1,38 @@
 package zadaci_12_02_2018;
 
+import java.util.*;
 public class NajvecaVrijednostArrayList {
 
-	public NajvecaVrijednostArrayList() {
+	public static void main(String[] args) {
+		Scanner unos = new Scanner(System.in);
+		
+		ArrayList<Integer> list = new ArrayList<>();
+		list.add(5);
+		list.add(4);
+		list.add(3);
+		list.add(2);
+		list.add(1);
+		
+		if (max(list) == null){
+			System.out.println("Lista je prazna");
+		}
+		else {
+		System.out.println("Najveci broj u listi je " + max(list));
+		}
 	}
-
+public static Integer max(ArrayList<Integer>list){
+	if (list == null || list.size() == 0){
+		return null;
+	}
+	else {
+		int max = list.get(0);
+		for (int e : list){
+			if (e > max){
+				max = e;
+			}
+		}
+	
+	return max;
+	}
+}
 }
--- src/zadaci_12_02_2018/RandomBrojevi.java
+++ src/zadaci_12_02_2018/RandomBrojevi.java
@@ -1,9 +1,26 @@
 package zadaci_12_02_2018;
 
+import java.util.Scanner;
 public class RandomBrojevi {
 
-	public RandomBrojevi() {
-
+	public static void main(String[] args) {
+		Scanner unos = new Scanner(System.in);
+		
+		int[] list = new int[100];
+		int[] brojac = new int[10];
+		
+		for (int i = 1; i < 100; i++){
+			list[i] = (int)(Math.random() * 10);
+		}
+		for (int i = 0; i < brojac.length; i++){
+			brojac[i] = 0;
+		}
+		for (int i = 0; i < list.length; i++){
+			brojac[list[i]]++;
+		}
+		for (int i = 0; i < brojac.length; i++){
+			System.out.println("Broj " + i + " se ponovio " + brojac[i] + " puta.");
+		}
 	}
 
 }
